fe1e9096239325ddf86b5ce06bd35497537b4acc
zTitiK\tungthanh.1497 <tungthanh.1497@gmail.com>
2017-07-24 02:27:28 +0700

    part 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
+var hotties = require('./models/hottie');
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,8 +13,23 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// cu khi nao user truy cap vao /api thi function nay se dc goi
+// res = response
+app.get('/api', function(req, res){
+    res.json({'hello': 'world'});
+  }
+);
+
+app.get('/api/gxtg', function(req, res){
+    res.json(hotties);
+  }
+);
+
+app.post('/api/gxtg', function(req, res)){
+  res.json({'post': 'API POST'})
+};
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ models/hottie.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "Linh Ca",
+    age: 16,
+    image:"http://kenh14cdn.com/2017/16105485-1455844831123077-5879960679786806858-n-1497890356348.jpg"
+  },
+  {
+    name: "Quân Kun",
+    age: 21,
+    image: "http://www.xaluan.com/images/news/Image/2014/02/15/quan-kun-_vn1.jpg"
+  },
+  {
+    name: "Lệ Rơi",
+    age: 27,
+    image:"https://i.ytimg.com/vi/tAKXiqwUC6Q/hqdefault.jpg"
+  }
+];
