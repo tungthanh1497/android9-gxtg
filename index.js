65ddd864ec53226292c5d53fda5a7e9d35ea5937
zTitiK\tungthanh.1497 <tungthanh.1497@gmail.com>
2017-07-24 03:23:54 +0700

    no message

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
-var hotties = require('./models/hottie');
+var Hottie = require('./models/hottie');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
+
+mongoose.connect('mongodb://admin:admin@ds119223.mlab.com:19223/android9-gxtg'
+, {useMongoClient: true});
+
+var hottie = new Hottie({
+    name: "Linh Ca",
+    age: 16,
+    image:"http://kenh14cdn.com/2017/16105485-1455844831123077-5879960679786806858-n-1497890356348.jpg"
+  });
+
+hottie.save();
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -25,9 +41,22 @@ app.get('/api/gxtg', function(req, res){
   }
 );
 
-app.post('/api/gxtg', function(req, res)){
-  res.json({'post': 'API POST'})
-};
+app.post('/api/gxtg', function(req, res){
+  var body = req.body;
+
+  var name = body.name;
+  var age = body.age;
+  var image = body.image;
+
+hotties.push({
+  name:name,
+  age:age,
+  image:image
+});
+
+  res.json({"success":"1"});
+  }
+);
 
 
 app.listen(app.get('port'), function() {
